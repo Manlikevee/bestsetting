e28b7cfacff380a88cfd97bed4416e99ca3634f0
manlikevee <odahviktor@gmail.com>
2022-05-14 15:10:40 +0100

    pushing

---
+++ dashboard-master/shift.js
@@ -0,0 +1,18 @@
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tc) => {
+      tc.classList.remove("is-active");
+    });
+    target.classList.add("is-active");
+
+    tabs.forEach((t) => {
+      t.classList.remove("is-active");
+    });
+    tab.classList.add("is-active");
+  });
+});
